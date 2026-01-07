0eef3c7106cb575105f753d53e158e99393826df
Sanika <sanikakhire891@gmail.com>
2026-01-07 12:53:41 +0530

    New Added

---
+++ environment/variable.tf
@@ -0,0 +1,10 @@
+variable "rg" {
+    description = "resource group name"
+    default = "Git-rg"
+  
+}
+
+variable "location" {
+     description = "location of resource group"
+     default = "centralindia"
+}
