c9376c518451d523062876c4e520468a2bbf9637
Sanika <sanikakhire891@gmail.com>
2026-01-12 11:52:27 +0530

    Backend added manually

--- environment/backend.tf
+++ environment/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "Git-rg"
-    storage_account_name = "backendstatefilegit"
-    container_name = "tfstate" 
+    resource_group_name  = "Backend-git"
+    storage_account_name = "storageaccountbackendgit"
+    container_name = "backendgit" 
     key = "terraform.tfstate"
   }
 }
